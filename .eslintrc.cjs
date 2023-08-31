172bedaed84d4b904917a7604d034179588d2d68
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-31 12:39:41 +0300

    feat: init app

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      },
+    ],
   },
 }
