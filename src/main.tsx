8661cc4a6f63f764e4281bb98adb46b4a0e64e0f
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-10 17:46:19 +0300

    feat: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
