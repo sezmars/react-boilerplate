eb560d2b5c00a7287daedc6f4ada5cfbb6c8bed7
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-10 17:47:07 +0300

    feat: add services

---
+++ src/services/apiSettings.ts
@@ -0,0 +1,32 @@
+import supabase from './supabase.js';
+import { ISettings } from '../interfaces/settings.ts';
+import { Api } from '../utils/enums.ts';
+
+export const getSettings = async (): Promise<ISettings> => {
+  const { data, error } = await supabase
+    .from(Api.settings)
+    .select('*')
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error('Settings could not be loaded');
+  }
+  return data;
+};
+
+// We expect a newSetting object that looks like {setting: newValue}
+export const updateSetting = async (newSetting: ISettings) => {
+  const { data, error } = await supabase
+    .from(Api.settings)
+    .update(newSetting)
+    // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
+    .eq('id', 1)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error('Settings could not be updated');
+  }
+  return data;
+};
