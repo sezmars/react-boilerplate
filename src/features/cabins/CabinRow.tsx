b91d71f8a6b26a749e26d00e031f5df630907263
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-10 16:13:05 +0300

    feat: add cabins features

---
+++ src/features/cabins/CabinRow.tsx
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+import { ICabin } from '../../interfaces/cabin.ts';
+import { formatCurrency } from '../../utils/helpers.ts';
+import CabinForm from './CabinForm.tsx';
+import { useDeleteCabin } from './useDeleteCabin.ts';
+import { HiSquare2Stack, HiTrash, HiPencil } from 'react-icons/hi2';
+import { useCreateCabin } from './useCreateCabin.ts';
+import Modal from '../../ui/Modal.tsx';
+import ConfirmDelete from '../../ui/ConfirmDelete.tsx';
+import Menus from '../../ui/Menus.tsx';
+import Table from '../../ui/Table.tsx';
+
+const Img = styled.img`
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3 / 2;
+  object-fit: cover;
+  object-position: center;
+  transform: scale(1.5) translateX(-7px);
+`;
+
+const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: 'Sono';
+`;
+
+const Price = styled.div`
+  font-family: 'Sono';
+  font-weight: 600;
+`;
+
+const Discount = styled.div`
+  font-family: 'Sono';
+  font-weight: 500;
+  color: var(--color-green-700);
+`;
+
+export default function CabinRow({ cabin }: { cabin: ICabin }) {
+  const { isDeleting, deleteCabin } = useDeleteCabin();
+  const { createCabin } = useCreateCabin();
+
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    description,
+  } = cabin;
+
+  const handleDuplicate = () => {
+    createCabin({
+      name: `Copy of ${name}`,
+      maxCapacity,
+      regularPrice,
+      discount,
+      image,
+      description,
+    });
+  };
+
+  return (
+    <Table.Row>
+      <Img src={image} alt={name} />
+      <Cabin>{name}</Cabin>
+      <div>First up to {maxCapacity} guest</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      {discount ? (
+        <Discount>{formatCurrency(discount)}</Discount>
+      ) : (
+        <span>&mdash;</span>
+      )}
+      <div>
+        <Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+
+            <Menus.List id={cabinId}>
+              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+                Duplicate
+              </Menus.Button>
+
+              <Modal.Open opens="edit">
+                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              </Modal.Open>
+
+              <Modal.Open opens="delete">
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+              </Modal.Open>
+            </Menus.List>
+
+            <Modal.Window name="edit">
+              <CabinForm cabinToEdit={cabin} />
+            </Modal.Window>
+
+            <Modal.Window name="delete">
+              <ConfirmDelete
+                resourceName="cabins"
+                disabled={isDeleting}
+                onConfirm={() => deleteCabin(cabinId)}
+              />
+            </Modal.Window>
+          </Menus.Menu>
+        </Modal>
+      </div>
+    </Table.Row>
+  );
+}
---
+++ src/features/cabins/CabinTable.tsx
@@ -0,0 +1,58 @@
+import Spinner from '../../ui/Spinner.tsx';
+import CabinRow from './CabinRow.tsx';
+import { useCabins } from './useCabins.ts';
+import Menus from '../../ui/Menus.tsx';
+import Table from '../../ui/Table.tsx';
+import { useSearchParams } from 'react-router-dom';
+import Empty from '../../ui/Empty.tsx';
+import { FilterCabin } from '../../utils/enums.ts';
+
+const CabinTable = () => {
+  const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
+
+  if (isLoading) return <Spinner />;
+  if (!cabins?.length) return <Empty resourceName="cabins" />;
+
+  // 1) FILTER
+  const filterValue = searchParams.get('discount') || 'all';
+
+  let filteredCabins;
+  if (filterValue === FilterCabin.all) filteredCabins = cabins;
+  if (filterValue === FilterCabin.noDiscount)
+    filteredCabins = cabins?.filter(cabin => cabin.discount === 0);
+  if (filterValue === FilterCabin.withDiscount)
+    filteredCabins = cabins?.filter(cabin => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins?.sort(
+    (a, b) => ((a as never)[field] - (b as never)[field]) * modifier
+  );
+
+  return (
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+
+        <Table.Body
+          // data={cabins}
+          // data={filteredCabins}
+          data={sortedCabins}
+          render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
+  );
+};
+
+export default CabinTable;
