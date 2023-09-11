eda0eb37205fb77125dbea87c49a762120aa9db4
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 17:10:59 +0300

    Updates

---
+++ assets/index-7e89d15d.js
@@ -0,0 +1,1 @@
+import{i as a}from"./apiBookings-b98ce200.js";import{r as i}from"./index-a84ab2bb.js";function s(r){return i(1,arguments),a(r,Date.now())}export{s as i};
--- assets/useMoveBack-9577d278.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index-a71a39d0.js";const o=()=>{const a=e();return()=>a(-1)};export{o as u};
