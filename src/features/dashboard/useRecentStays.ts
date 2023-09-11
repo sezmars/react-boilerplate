094b6630976f94f5aabf0cdc01a2c0c25e9eabf4
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 11:16:07 +0300

    refactor: add enum

--- src/features/dashboard/useRecentStays.ts
+++ src/features/dashboard/useRecentStays.ts
@@ -2,13 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { subDays } from 'date-fns';
 import { useSearchParams } from 'react-router-dom';
 import { getStaysAfterDate } from '../../services/apiBookings';
+import { Status } from '../../utils/enums.ts';
 
 export const useRecentStays = () => {
   const [searchParams] = useSearchParams();
 
-  const numDays = !searchParams.get('last')
-    ? 7
-    : Number(searchParams.get('last'));
+  const numDays = !searchParams.get('last') ? 7 : Number(searchParams.get('last'));
   const queryDate = subDays(new Date(), numDays).toISOString();
 
   const { isLoading, data: stays } = useQuery({
@@ -16,9 +15,7 @@ export const useRecentStays = () => {
     queryKey: ['stays', `last-${numDays}`],
   });
 
-  const confirmedStays = stays?.filter(
-    stay => stay.status === 'checked-in' || stay.status === 'checked-out'
-  );
+  const confirmedStays = stays?.filter((stay) => stay.status === Status.checkedIn || stay.status === Status.checkedOut);
 
   return { isLoading, stays, confirmedStays, numDays };
 };
