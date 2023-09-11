05c2375c43261a4d5495bd033887b2dc8bc9897f
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 15:50:38 +0300

    fix: return heigth

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -7,6 +7,7 @@ const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
+  height: 100vh;
 
   @media (max-width: 768px) {
     grid-template-columns: 6rem 1fr;
