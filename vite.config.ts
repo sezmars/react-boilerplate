0431526c5f61d21282bb9d11b71e72f6ac847d44
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 15:38:59 +0300

    refactor: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/react-boilerplate',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  console.log(command);
+  const config = {
+    plugins: [react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/react-boilerplate/';
+  }
+
+  return config;
 });
