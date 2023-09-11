2ac380c43eff67a8944b8d8eb9076d7e545b10ff
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 14:01:23 +0300

    Updates

---
+++ assets/Flag-a70ccca9.js
@@ -0,0 +1,6 @@
+import{s as r}from"./index-0f9aff97.js";const a=r.img`
+  max-width: 2rem;
+  border-radius: var(--border-radius-tiny);
+  display: block;
+  border: 1px solid var(--color-grey-100);
+`;export{a as F};
---
+++ assets/useMoveBack-f5ba0110.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-0f9aff97.js";const o=()=>{const a=e();return()=>a(-1)};export{o as u};
