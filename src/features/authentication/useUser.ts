7425062b1cf027c7473e203d944766bf0cad150f
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 11:13:26 +0300

    refactor: update types

--- src/features/authentication/useUser.ts
+++ src/features/authentication/useUser.ts
@@ -1,28 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCurrentUser } from '../../services/apiAuth';
 import { User } from '@supabase/supabase-js';
-
-interface IUserCheck {
-  user_metadata: { fullName: string; avatar: string };
-  email: string;
-}
+import { Api } from '../../utils/enums.ts';
 
 export const useUser = (): {
   isLoading: boolean;
-  user: Partial<User | IUserCheck>;
+  user: User | null | undefined;
   isAuthenticated: boolean;
 } => {
   const { isLoading, data: user } = useQuery({
-    queryKey: ['user'],
+    queryKey: [Api.user],
     queryFn: getCurrentUser,
   });
 
   return {
     isLoading,
-    user: user || {
-      user_metadata: { fullName: '' },
-      email: '',
-    },
+    user,
     isAuthenticated: user?.role === 'authenticated',
   };
 };
