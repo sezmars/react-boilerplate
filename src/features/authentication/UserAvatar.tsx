bea22e2ee5437e23d72c76c714f4b0523a2260c9
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-11 11:13:12 +0300

    refactor: update types

--- src/features/authentication/UserAvatar.tsx
+++ src/features/authentication/UserAvatar.tsx
@@ -23,14 +23,11 @@ const Avatar = styled.img`
 
 const UserAvatar = () => {
   const { user } = useUser();
-  const { fullName, avatar } = user.user_metadata!;
+  const { fullName, avatar } = user!.user_metadata;
 
   return (
     <StyledUserAvatar>
-      <Avatar
-        src={avatar || 'default-user.jpg'}
-        alt={`Avatar of ${fullName}`}
-      />
+      <Avatar src={avatar || 'default-user.jpg'} alt={`Avatar of ${fullName}`} />
       <span>{fullName}</span>
     </StyledUserAvatar>
   );
