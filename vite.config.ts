cb875979917a8516015ed809d8769639fb3dc7c2
Roman <mr.sezmars@gmail.com>
2023-10-02 23:18:30 +0300

    Update vite.config.ts
    clear

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  console.log(command);
   const config = {
     plugins: [react()],
     base: '/',
